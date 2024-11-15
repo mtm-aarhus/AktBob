2a10e6bf2bdf8c9a5c1e4eca986f7088a4b5df99
Jakob Nørager Jørgensen <jojan@aarhus.dk>
2024-11-15 15:28:01 +0100

    Add caching of Deskpro person data

--- AktBob.Deskpro/GetDeskproPersonQueryHandler.cs
+++ AktBob.Deskpro/GetDeskproPersonQueryHandler.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using AktBob.Deskpro.Contracts;
 using AktBob.Deskpro.Contracts.DTOs;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace AktBob.Deskpro;
 internal class GetDeskproPersonQueryHandler : IRequestHandler<GetDeskproPersonQuery, Result<PersonDto>>
@@ -13,17 +14,28 @@ internal class GetDeskproPersonQueryHandler : IRequestHandler<GetDeskproPersonQu
     private readonly IConfiguration _configuration;
     private readonly ILogger<GetDeskproPersonQueryHandler> _logger;
     private readonly IMediator _mediator;
+    private readonly IMemoryCache _cache;
+    private const string CACHE_KEY = "DeskproPerson";
 
-    public GetDeskproPersonQueryHandler(IDeskproClient deskpro, IConfiguration configuration, ILogger<GetDeskproPersonQueryHandler> logger, IMediator mediator)
+    public GetDeskproPersonQueryHandler(IDeskproClient deskpro, IConfiguration configuration, ILogger<GetDeskproPersonQueryHandler> logger, IMediator mediator, IMemoryCache cache)
     {
         _deskpro = deskpro;
         _configuration = configuration;
         _logger = logger;
         _mediator = mediator;
+        _cache = cache;
     }
 
     public async Task<Result<PersonDto>> Handle(GetDeskproPersonQuery request, CancellationToken cancellationToken)
     {
+        if (_cache.TryGetValue(CACHE_KEY + request.PersonId.ToString(), out PersonDto? dto))
+        {
+            if (dto != null)
+            {
+                return Result.Success(dto);
+            }
+        }
+
         var person = await _deskpro.GetPersonById(request.PersonId);
 
         if (person is null)
@@ -31,7 +43,7 @@ internal class GetDeskproPersonQueryHandler : IRequestHandler<GetDeskproPersonQu
             return Result.NotFound();
         }
 
-        var dto = new PersonDto
+        dto = new PersonDto
         {
             Id = person.Id,
             IsAgent = person.IsAgent,
@@ -42,6 +54,8 @@ internal class GetDeskproPersonQueryHandler : IRequestHandler<GetDeskproPersonQu
             FullName = person.FullName,
             PhoneNumbers = person.PhoneNumbers
         };
+
+        _cache.Set(CACHE_KEY + request.PersonId, dto, TimeSpan.FromHours(24));
 
         return Result.Success(dto);
     }
