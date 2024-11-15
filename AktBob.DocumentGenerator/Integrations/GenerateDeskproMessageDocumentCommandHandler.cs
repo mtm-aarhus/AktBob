9f4758f058107c41de5c405a8dc606cee9745fc9
Jakob Nørager Jørgensen <jojan@aarhus.dk>
2024-11-15 17:01:18 +0100

    Refactor message pdf render code

--- AktBob.DocumentGenerator/Integrations/GenerateDeskproMessageDocumentCommandHandler.cs
+++ AktBob.DocumentGenerator/Integrations/GenerateDeskproMessageDocumentCommandHandler.cs
@@ -5,7 +5,6 @@ using Unit = MigraDoc.DocumentObjectModel.Unit;
 using Document = MigraDoc.DocumentObjectModel.Document;
 using MigraDoc.Rendering;
 using AktBob.DocumentGenerator.Contracts;
-using AktBob.DocumentGenerator;
 using AktBob.DocumentGenerator.UseCases.GenerateMessageContent;
 
 namespace AktBob.DocumentGenerator.Integrations;
@@ -42,60 +41,6 @@ internal class GenerateDeskproMessageDocumentCommandHandler(IMediator mediator) 
         {
             var generateMessageContentCommand = new GenerateMessageContentCommand(section, message);
             await _mediator.Send(generateMessageContentCommand, cancellationToken);
-
-            //// Add message number
-            //section.AddMessageNumber(
-            //    messageNumber: message.MessageNumber.ToString(),
-            //    messageId: message.MessageId.ToString(),
-            //    fontName: Constants.FONT_NAME_OPEN_SANS_SEMIBOLD,
-            //    fontSize: Unit.FromPoint(12),
-            //    fontColor: Color.Parse("0xFF000000"),
-            //    spaceAfter: Unit.FromPoint(12));
-
-            //// Add message timestamp
-            //section.AddMessageTimestamp(
-            //    text: message.CreatedAt.ToString("dd-MM-yyyy HH:mm:ss"),
-            //    fontName: Constants.FONT_NAME_OPEN_SANS,
-            //    fontSize: Unit.FromPoint(8),
-            //    fontColor: Color.Parse("0xffaaaaaa"));
-
-            //// Add "person from" name and email
-            //section.AddPerson(
-            //    name: message.PersonName,
-            //    email: message.PersonEmail,
-            //    fontName: Constants.FONT_NAME_OPEN_SANS,
-            //    fontNameEmphasis: Constants.FONT_NAME_OPEN_SANS_SEMIBOLD,
-            //    fontSize: Unit.FromPoint(8),
-            //    fontColor: Color.Parse("0xffaaaaaa"),
-            //    spaceAfter: Unit.FromPoint(2));
-
-            //section.AddHorizontalLine(
-            //    width: Unit.FromPoint(0.4f),
-            //    color: Color.Parse("0xFFBBBBBB"),
-            //    spaceBefore: Unit.FromPoint(10),
-            //    spaceAfter: Unit.FromPoint(10));
-
-            //section.ProcessMessageText(
-            //    html: message.MessageContent,
-            //    fontName: Constants.FONT_NAME_OPEN_SANS,
-            //    fontSize: Unit.FromPoint(9),
-            //    fontColor: Color.Parse("0xff000000"),
-            //    linkColor: Color.Parse("0xFF4444FF"),
-            //    spaceAfter: Unit.FromPoint(12));
-
-            //// Attachments
-            //section.AddHeadline("Bilag", Constants.FONT_NAME_OPEN_SANS_SEMIBOLD, Unit.FromPoint(7), Color.Parse("0xff000000"), Unit.FromPoint(0));
-
-            //section.AddAttachmentList(
-            //    filenames: message.AttachmentFileNames,
-            //    fontName: Constants.FONT_NAME_OPEN_SANS,
-            //    fontSize: Unit.FromPoint(7),
-            //    fontColor: Color.Parse("0xff555555"),
-            //    spaceAfter: Unit.FromPoint(2));
-
-            //var spacer = section.AddParagraph();
-            //spacer.Format.Font.Name = Constants.FONT_NAME_OPEN_SANS;
-            //spacer.Format.SpaceAfter = Unit.FromPoint(24);
         }
 
         // Render the document as a PDF
