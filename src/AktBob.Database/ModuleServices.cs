92be145fa4503e8371a23c9eeed469247b5b7256
Jakob Jørgensen <jakob@jakobjorgensen.com>
2025-03-02 15:07:27 +0100

    Change database repository to a scoped service

--- src/AktBob.Database/ModuleServices.cs
+++ src/AktBob.Database/ModuleServices.cs
@@ -1,25 +1,27 @@
 ﻿using AktBob.Database.Contracts;
 using AktBob.Database.Repositories;
 using Ardalis.GuardClauses;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Data;
 
 namespace AktBob.Database;
 public static class ModuleServices
 {
     public static IServiceCollection AddDatabaseModule(this IServiceCollection services, IConfiguration configuration)
     {
-        Guard.Against.NullOrEmpty(configuration.GetConnectionString("Database"));
+        var connectionString = Guard.Against.NullOrEmpty(configuration.GetConnectionString("Database"));
 
-        services.AddTransient<ISqlDataAccess, SqlDataAccess>();
+        services.AddScoped<IDbConnection>(x => new SqlConnection(connectionString));
+        services.AddScoped<ISqlDataAccess, SqlDataAccess>();
 
         // Repositories
-        services.AddTransient<IMessageRepository, MessageRepository>();
-        services.AddTransient<ITicketRepository, TicketRepository>();
-        services.AddTransient<ICaseRepository, CaseRepository>();
-        services.AddTransient<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IMessageRepository, MessageRepository>();
+        services.AddScoped<ITicketRepository, TicketRepository>();
+        services.AddScoped<ICaseRepository, CaseRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
         
-
         return services;
     }
 }
--- src/AktBob.Database/SqlDataAccess.cs
+++ src/AktBob.Database/SqlDataAccess.cs
@@ -1,68 +1,19 @@
-﻿using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Configuration;
-using System.Data;
+﻿using System.Data;
 
 namespace AktBob.Database;
 
 internal class SqlDataAccess : ISqlDataAccess
 {
-    private readonly IConfiguration _configuration;
+    private readonly IDbConnection _connection;
 
-    public SqlDataAccess(IConfiguration configuration)
+    public SqlDataAccess(IDbConnection connection)
     {
-        _configuration = configuration;
+        _connection = connection;
     }
 
-    private string GetConnectionString() => _configuration.GetConnectionString("Database")!;
-
-    public async Task<T?> QuerySingle<T>(string sql, object? parameters)
-    {
-        using (var connection = new SqlConnection(GetConnectionString()))
-        {
-            var entity = await connection.QuerySingleOrDefaultAsync<T>(sql, parameters, commandType: CommandType.Text);
-            return entity;
-        }
-    }
-
-    public async Task<IEnumerable<T>> Query<T>(string sql, object? parameters)
-    {
-        using (var connection = new SqlConnection(GetConnectionString()))
-        {
-            return await connection.QueryAsync<T>(sql, parameters, commandType: CommandType.Text);
-        }
-    }
-
-    public async Task<IEnumerable<T>> Query<T, U>(string sql, object parameters, string splitOn, Func<T, U, T> map)
-    {
-        using (var connection = new SqlConnection(GetConnectionString()))
-        {
-            return await connection.QueryAsync(
-                sql: sql,
-                map: map,
-                param: parameters,
-                splitOn: splitOn,
-                commandType: CommandType.Text);
-        }
-    }
-
-
-    public async Task<int> ExecuteProcedure(string procedureName, DynamicParameters? parameters)
-    {
-        var connectionString = GetConnectionString();
-
-        using (var connection = new SqlConnection(connectionString))
-        {
-            return await connection.ExecuteAsync(procedureName, parameters, commandType: CommandType.StoredProcedure);
-        }
-    }
-
-    public async Task<int> Execute<T>(string sql, T? parameters)
-    {
-        var connectionString = GetConnectionString();
-
-        using (var connection = new SqlConnection(connectionString))
-        {
-            return await connection.ExecuteAsync(sql, parameters, commandType: CommandType.Text);
-        }
-    }
+    public async Task<T?> QuerySingle<T>(string sql, object? parameters) => await _connection.QuerySingleOrDefaultAsync<T>(sql, parameters, commandType: CommandType.Text);
+    public async Task<IEnumerable<T>> Query<T>(string sql, object? parameters) => await _connection.QueryAsync<T>(sql, parameters, commandType: CommandType.Text);
+    public async Task<IEnumerable<T>> Query<T, U>(string sql, object parameters, string splitOn, Func<T, U, T> map) => await _connection.QueryAsync(sql: sql, map: map, param: parameters, splitOn: splitOn, commandType: CommandType.Text);
+    public async Task<int> Execute<T>(string sql, T? parameters) => await _connection.ExecuteAsync(sql, parameters, commandType: CommandType.Text);
+    public async Task<int> ExecuteProcedure(string procedureName, DynamicParameters? parameters) => await _connection.ExecuteAsync(procedureName, parameters, commandType: CommandType.StoredProcedure);
 }
