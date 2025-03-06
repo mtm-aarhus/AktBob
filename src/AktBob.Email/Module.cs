81a8a19ace8e07ded99f469d321246a74130a89d
Jakob Nørager Jørgensen <jojan@aarhus.dk>
2025-03-06 12:58:54 +0100

    Converting email subject and body to and from base64

--- src/AktBob.Email/Module.cs
+++ src/AktBob.Email/Module.cs
@@ -1,9 +1,19 @@
 ﻿using AktBob.Email.Contracts;
 using AktBob.Shared;
+using System.Text;
 
 namespace AktBob.Email;
 
 internal class Module(IJobDispatcher jobDispatcher) : IEmailModule
 {
-    public void Send(string to, string subject, string body) => jobDispatcher.Dispatch(new SendEmailJob(to, subject, body));
+    public void Send(string to, string subject, string body)
+    {
+        var subjectBytes = Encoding.UTF8.GetBytes(subject);
+        var base64Subject = Convert.ToBase64String(subjectBytes);
+
+        var bodyBytes = Encoding.UTF8.GetBytes(body);
+        var base64Body = Convert.ToBase64String(bodyBytes);
+
+        jobDispatcher.Dispatch(new SendEmailJob(to, base64Subject, base64Body));
+    }
 }
--- src/AktBob.Email/SendEmailJobHandler.cs
+++ src/AktBob.Email/SendEmailJobHandler.cs
@@ -3,16 +3,20 @@ using Ardalis.GuardClauses;
 using MailKit.Net.Smtp;
 using Microsoft.Extensions.Configuration;
 using MimeKit;
+using System.Text;
 
 namespace AktBob.Email;
 
-internal record SendEmailJob(string To, string Subject, string Body);
+internal record SendEmailJob(string To, string Base64Subject, string Base64Body);
 internal class SendEmailJobHandler(IConfiguration configuration) : IJobHandler<SendEmailJob>
 {
     private readonly IConfiguration _configuration = configuration;
 
     public async Task Handle(SendEmailJob job, CancellationToken cancellationToken = default)
     {
+        var subject = Encoding.UTF8.GetString(Convert.FromBase64String(job.Base64Subject));
+        var body = Encoding.UTF8.GetString(Convert.FromBase64String(job.Base64Body));
+
         var smtpUrl = Guard.Against.NullOrEmpty(_configuration.GetValue<string>("EmailModule:SmtpUrl"));
         var smtpPort = _configuration.GetValue<int>("EmailModule:SmtpPort");
         var smtpUseSsl = _configuration.GetValue<bool?>("EmailModule:SmtpUseSsl") ?? false;
@@ -20,15 +24,15 @@ internal class SendEmailJobHandler(IConfiguration configuration) : IJobHandler<S
 
         using (SmtpClient smtpClient = new SmtpClient())
         {
-            smtpClient.Connect(smtpUrl, 25, false);
+            smtpClient.Connect(smtpUrl, smtpPort, smtpUseSsl);
 
             var message = new MimeMessage();
             message.From.Add(new MailboxAddress(from, from));
             message.To.Add(new MailboxAddress(job.To, job.To));
-            message.Subject = job.Subject;
+            message.Subject = subject;
             message.Body = new TextPart("plain")
             {
-                Text = job.Body
+                Text = body
             };
 
             await smtpClient.SendAsync(message);
