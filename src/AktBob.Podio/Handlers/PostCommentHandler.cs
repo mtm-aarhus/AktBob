5556a9afd8a6d67f337a84d94084200468ceb8b8
Jakob Jørgensen <jakob@jakobjorgensen.com>
2025-03-09 09:24:23 +0100

    Remove logging statements from Podio jobs

--- src/AktBob.Podio/Handlers/PostCommentHandler.cs
+++ src/AktBob.Podio/Handlers/PostCommentHandler.cs
@@ -1,19 +1,15 @@
 ﻿using AAK.Podio;
 using AktBob.Podio.Contracts;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace AktBob.Podio.Handlers;
-public class PostCommentHandler(IPodioFactory podioFactory, IConfiguration configuration, ILogger<PostCommentHandler> logger) : IPostCommentHandler
+public class PostCommentHandler(IPodioFactory podioFactory, IConfiguration configuration) : IPostCommentHandler
 {
     private readonly IPodioFactory _podioFactory = podioFactory;
     private readonly IConfiguration _configuration = configuration;
-    private readonly ILogger<PostCommentHandler> _logger = logger;
 
     public async Task Handle(PostCommentCommand command, CancellationToken cancellationToken)
     {
-        _logger.LogInformation("Posting comment on Podio item. ItemId {itemId} Value: '{value}'", command.PodioItemId.Id, command.TextValue);
-
         var podio = _podioFactory.Create(
             appId: command.PodioItemId.AppId,
             appToken: ConfigurationHelper.GetAppToken(_configuration, command.PodioItemId.AppId),
@@ -21,7 +17,5 @@ public class PostCommentHandler(IPodioFactory podioFactory, IConfiguration confi
             clientSecret: ConfigurationHelper.GetClientSecret(_configuration));
 
         await podio.PostItemComment(command.PodioItemId.AppId, command.PodioItemId.Id, command.TextValue, cancellationToken);
-
-        _logger.LogInformation("Comment posted on Podio item. ItemId {itemId} Value: '{value}'", command.PodioItemId.Id, command.TextValue);
     }
 }
--- src/AktBob.Podio/Handlers/UpdateTextFieldHandler.cs
+++ src/AktBob.Podio/Handlers/UpdateTextFieldHandler.cs
@@ -1,19 +1,15 @@
 ﻿using AAK.Podio;
 using AktBob.Podio.Contracts;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace AktBob.Podio.Handlers;
-internal class UpdateTextFieldHandler(IPodioFactory podioFactory, ILogger<UpdateTextFieldHandler> logger, IConfiguration configuration) : IUpdateTextFieldHandler
+internal class UpdateTextFieldHandler(IPodioFactory podioFactory, IConfiguration configuration) : IUpdateTextFieldHandler
 {
     private readonly IPodioFactory _podioFactory = podioFactory;
-    private readonly ILogger<UpdateTextFieldHandler> _logger = logger;
     private readonly IConfiguration _configuration = configuration;
 
     public async Task Handle(UpdateTextFieldCommand command, CancellationToken cancellationToken)
     {
-        _logger.LogInformation("Updating Podio item. ItemId {itemId} FielId {fieldId} Value: '{value}'", command.PodioItemId.Id, command.FieldId, command.TextValue);
-
         var podio = _podioFactory.Create(
             appId: command.PodioItemId.AppId,
             appToken: ConfigurationHelper.GetAppToken(_configuration, command.PodioItemId.AppId),
@@ -21,7 +17,5 @@ internal class UpdateTextFieldHandler(IPodioFactory podioFactory, ILogger<Update
             clientSecret: ConfigurationHelper.GetClientSecret(_configuration));
 
         await podio.UpdateItemField(command.PodioItemId.AppId, command.PodioItemId.Id, command.FieldId, command.TextValue, cancellationToken);
-
-        _logger.LogInformation("Podio item updated. ItemId {itemId} FieldId {fieldId} Value: '{value}'", command.PodioItemId.Id, command.FieldId, command.TextValue);
     }
 }
