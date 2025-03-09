3feed86dbd4eb6be9b33a071251a1543daf6fda1
Jakob Jørgensen <jakob@jakobjorgensen.com>
2025-03-09 09:21:41 +0100

    Validate Podio job parameters

--- src/AktBob.Podio/Jobs/PostComment.cs
+++ src/AktBob.Podio/Jobs/PostComment.cs
@@ -1,5 +1,6 @@
 ﻿using AktBob.Podio.Contracts;
 using AktBob.Shared;
+using Ardalis.GuardClauses;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AktBob.Podio.Jobs;
@@ -12,6 +13,10 @@ internal class PostComment(IServiceScopeFactory serviceScopeFactory) : IJobHandl
 
     public async Task Handle(PostCommentJob job, CancellationToken cancellationToken = default)
     {
+        Guard.Against.NegativeOrZero(job.PodioItemId.AppId);
+        Guard.Against.NegativeOrZero(job.PodioItemId.Id);
+        Guard.Against.NullOrEmpty(job.TextValue);
+
         using var scope = _serviceScopeFactory.CreateScope();
         var postPodioCommentHandler = scope.ServiceProvider.GetRequiredService<IPostCommentHandler>();
 
--- src/AktBob.Podio/Jobs/UpdateTextField.cs
+++ src/AktBob.Podio/Jobs/UpdateTextField.cs
@@ -1,5 +1,6 @@
 ﻿using AktBob.Podio.Contracts;
 using AktBob.Shared;
+using Ardalis.GuardClauses;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AktBob.Podio.Jobs;
@@ -12,6 +13,11 @@ internal class UpdateTextField(IServiceScopeFactory serviceScopeFactory) : IJobH
 
     public async Task Handle(UpdateTextFieldJob job, CancellationToken cancellationToken = default)
     {
+        Guard.Against.NegativeOrZero(job.PodioItemId.AppId);
+        Guard.Against.NegativeOrZero(job.PodioItemId.Id);
+        Guard.Against.NegativeOrZero(job.FieldId);
+        Guard.Against.NullOrEmpty(job.TextValue);
+
         using var scope = _serviceScopeFactory.CreateScope();
         var handler = scope.ServiceProvider.GetRequiredService<IUpdateTextFieldHandler>();
 
