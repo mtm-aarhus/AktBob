c65d8473ba1418c8b13a9467f04e488d79ca2dcb
Jakob Nørager Jørgensen <jojan@aarhus.dk>
2025-03-19 10:18:44 +0100

    Add tests for AktBob.Database.Extensions.MessageExtensions

---
+++ AktBob.Database.Tests.Unit/Extensions/MessageExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using AktBob.Database.Dtos;
+using AktBob.Database.Entities;
+using AktBob.Database.Extensions;
+using FluentAssertions;
+
+namespace AktBob.Database.Tests.Unit.Extensions;
+
+public class MessageExtensionsTests
+{
+    [Fact]
+    public void ToDto_ShouldReturnDto_WhenInvoked()
+    {
+        // Arrange
+        var message = new Message
+        {
+            DeskproMessageId = 123,
+            GODocumentId = 123,
+            Id = 123,
+            MessageNumber = 123,
+            TicketId = 123
+        };
+
+        // Act
+        var result = message.ToDto();
+
+        // Assert
+        result.Should().BeOfType(typeof(MessageDto));
+        result.DeskproMessageId.Should().Be(message.DeskproMessageId);
+        result.GODocumentId.Should().Be(message.GODocumentId);
+        result.Id.Should().Be(message.Id);
+        result.MessageNumber.Should().Be(message.MessageNumber);
+        result.TicketId.Should().Be(message.TicketId);
+    }
+}
