5a1d9a4af2a08552119074b203e9ac0ec768a4fc
Jakob Nørager Jørgensen <jojan@aarhus.dk>
2025-03-20 10:23:07 +0100

    Add unit tests for OpenOrchestrator module logging decorator

---
+++ AktBob.OpenOrchestrator.Tests.Unit/Decorators/ModuleLoggingDecoratorTests.cs
@@ -0,0 +1,33 @@
+﻿using AktBob.OpenOrchestrator.Contracts;
+using AktBob.OpenOrchestrator.Decorators;
+using FluentAssertions;
+using Microsoft.Extensions.Logging.Testing;
+using NSubstitute;
+
+namespace AktBob.OpenOrchestrator.Tests.Unit.Decorators;
+
+public class ModuleLoggingDecoratorTests
+{
+    private readonly ModuleLoggingDecorator _sut;
+    private readonly IOpenOrchestratorModule _inner = Substitute.For<IOpenOrchestratorModule>();
+    private readonly FakeLogger<OpenOrchestratorModule> _logger = new FakeLogger<OpenOrchestratorModule>();
+
+    public ModuleLoggingDecoratorTests()
+    {
+        _sut = new ModuleLoggingDecorator(_inner, _logger);
+    }
+
+    [Fact]
+    public void CreateQueueItem_ShouldLogInformationAndInvokeInnerModule_WhenInvoked()
+    {
+        // Arrange
+        var command = new CreateQueueItemCommand(string.Empty, string.Empty, string.Empty);
+
+        // Act
+        _sut.CreateQueueItem(command);
+
+        // Assert
+        _logger.Collector.LatestRecord.Level.Should().Be(Microsoft.Extensions.Logging.LogLevel.Information);
+        _inner.Received(1).CreateQueueItem(command);
+    }
+}
