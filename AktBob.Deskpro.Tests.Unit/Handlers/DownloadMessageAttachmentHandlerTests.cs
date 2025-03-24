94251ca4c1731fcf2805ea13a257af7f7770c99c
Jakob Nørager Jørgensen <jojan@aarhus.dk>
2025-03-24 12:22:50 +0100

    Add test for Deskpro DownloadMessageAttachmentHandler

---
+++ AktBob.Deskpro.Tests.Unit/Handlers/DownloadMessageAttachmentHandlerTests.cs
@@ -0,0 +1,77 @@
+﻿using AAK.Deskpro;
+using AktBob.Deskpro.Handlers;
+using FluentAssertions;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using NSubstitute.ReturnsExtensions;
+using System.Text;
+
+namespace AktBob.Deskpro.Tests.Unit.Handlers;
+public class DownloadMessageAttachmentHandlerTests
+{
+    private readonly DownloadMessageAttachmentHandler _sut;
+    private readonly IDeskproClient _deskproClient = Substitute.For<IDeskproClient>();
+
+    public DownloadMessageAttachmentHandlerTests()
+    {
+        _sut = new DownloadMessageAttachmentHandler(_deskproClient);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnStreamResult_WhenDeskproClientResponseIsSuccessful()
+    {
+        // Arrange
+        var bytes = Encoding.UTF8.GetBytes("some value");
+        using var stream = new MemoryStream(bytes);
+        _deskproClient.DownloadAttachment(Arg.Any<string>(), Arg.Any<CancellationToken>()).Returns(stream);
+
+        // Act
+        var result = await _sut.Handle(string.Empty, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().BeSameAs(stream);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnErrorResultWithMessage_WhenDeskproClientResponseIsNull()
+    {
+        // Arrange
+        _deskproClient.DownloadAttachment(Arg.Any<string>(), Arg.Any<CancellationToken>()).ReturnsNull();
+
+        // Act
+        var result = await _sut.Handle(string.Empty, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        result.Errors.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnErrorResultWithMessage_WhenDeskproClientThrowsHttpRequestException()
+    {
+        // Arrange
+        _deskproClient.DownloadAttachment(Arg.Any<string>(), Arg.Any<CancellationToken>()).ThrowsAsync<HttpRequestException>();
+
+        // Act
+        var act = () => _sut.Handle(string.Empty, CancellationToken.None);
+        var result = await act();
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        result.Errors.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public async Task Handle_ShouldRethrowException_WhenDeskproClientThrowsAnyExceptionOtherThanHttpRequestException()
+    {
+        // Arrange
+        _deskproClient.DownloadAttachment(Arg.Any<string>(), Arg.Any<CancellationToken>()).ThrowsAsync<Exception>();
+
+        // Act
+        var act = () => _sut.Handle(string.Empty, CancellationToken.None);
+
+        // Assert
+        await act.Should().ThrowAsync<Exception>();
+    }
+}
