2ff602dd75408f5bd3bb764252dcb1e265f67904
Jakob Nørager Jørgensen <jojan@aarhus.dk>
2025-04-04 09:36:44 +0200

    Add sting extension to parse Deskpro datetime values

--- src/AktBob.Workflows/Extensions/StringExtensions.cs
+++ src/AktBob.Workflows/Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace AktBob.Workflows.Extensions;
 internal static class StringExtensions
@@ -22,5 +23,25 @@ internal static class StringExtensions
         string pattern = @"^[A-Za-z]\d{4}-\d{1,10}$";
         Regex regex = new Regex(pattern);
         return regex.IsMatch(caseNumber);
+    }
+
+    public static bool TryParseDeskproDateTime(this string? input, out DateTime? parsedDateTime)
+    {
+        var dateFormat = "yyyy-MM-dd'T'HH:mm:ssK";
+        var alternateDateFormat = "yyyy-MM-dd'T'HH:mm:ss+zzzz";  // Alternate format with no colon in offset
+
+        if (DateTime.TryParseExact(input, dateFormat, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out var date))
+        {
+            parsedDateTime = date;
+            return true;
+        }
+        else if (DateTime.TryParseExact(input, alternateDateFormat, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out date))
+        {
+            parsedDateTime = date;
+            return true;
+        }
+
+        parsedDateTime = null;
+        return false;
     }
 }
