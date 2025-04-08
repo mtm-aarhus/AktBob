92bb49f82c1b924decaab2b9797081c946fe51f8
Jakob Nørager Jørgensen <jojan@aarhus.dk>
2025-04-08 10:53:44 +0200

    Remove log statements from caching decorator

--- src/AktBob.Deskpro/Decorators/ModuleCachingDecorator.cs
+++ src/AktBob.Deskpro/Decorators/ModuleCachingDecorator.cs
@@ -5,13 +5,11 @@ namespace AktBob.Deskpro.Decorators;
 internal class ModuleCachingDecorator : IDeskproModule
 {
     private readonly IDeskproModule _inner;
-    private readonly ILogger<DeskproModule> _logger;
     private readonly ICacheService _cache;
 
-    public ModuleCachingDecorator(IDeskproModule inner, ILogger<DeskproModule> logger, ICacheService cache)
+    public ModuleCachingDecorator(IDeskproModule inner, ICacheService cache)
     {
         _inner = inner;
-        _logger = logger;
         _cache = cache;
     }
 
@@ -27,14 +25,12 @@ internal class ModuleCachingDecorator : IDeskproModule
         var cachedCustomSpecifications = _cache.Get<IReadOnlyCollection<CustomFieldSpecificationDto>?>(cacheKey);
         if (cachedCustomSpecifications != null && cachedCustomSpecifications.Any())
         {
-            _logger.LogDebug("Got Deskpro custom field specifications from cache");
             return Result.Success(cachedCustomSpecifications);
         }
 
         var result = await _inner.GetCustomFieldSpecifications(cancellationToken);
         if (result.IsSuccess)
         {
-            _logger.LogDebug("Setting cached value: Deskpro custom field specifications");
             _cache.Set(cacheKey, result.Value, TimeSpan.FromDays(5));
         }
 
@@ -48,14 +44,12 @@ internal class ModuleCachingDecorator : IDeskproModule
         
         if (cachedMessage != null)
         {
-            _logger.LogDebug("Got Deskpro message from cache (ticket {ticketId} message {messageId})", ticketId, messageId);
             return cachedMessage;
         }
 
         var result = await _inner.GetMessage(ticketId, messageId, cancellationToken);
         if (result.IsSuccess)
         {
-            _logger.LogDebug("Setting cached value: Deskpro ticket {ticketId} message {messageId}", ticketId, messageId);
             _cache.Set(cacheKey, result.Value, TimeSpan.FromDays(5));
         }
 
@@ -68,14 +62,12 @@ internal class ModuleCachingDecorator : IDeskproModule
         var cachedMessageAttachments = _cache.Get<IReadOnlyCollection<AttachmentDto>>(cacheKey);
         if (cachedMessageAttachments != null)
         {
-            _logger.LogDebug("Got Deskpro message attachment from cache (ticket {ticketId} message {messageId}", ticketId, messageId);
             return Result.Success(cachedMessageAttachments);
         }
 
         var result = await _inner.GetMessageAttachments(ticketId, messageId, cancellationToken);
         if (result.IsSuccess)
         {
-            _logger.LogDebug("Setting cached value: Deskpro message attachments (ticket {ticketId} message {messageId})", ticketId, messageId);
             _cache.Set(cacheKey, result.Value, TimeSpan.FromDays(5));
         }
 
@@ -95,7 +87,7 @@ internal class ModuleCachingDecorator : IDeskproModule
         var result = await _inner.GetPerson(personId, cancellationToken);
         if (result.IsSuccess)
         {
-            SetCachedPerson(cacheKey, result.Value);
+            _cache.Set(cacheKey, result.Value, TimeSpan.FromDays(20));
         }
 
         return result;
@@ -114,15 +106,9 @@ internal class ModuleCachingDecorator : IDeskproModule
         var result = await _inner.GetPerson(email, cancellationToken);
         if (result.IsSuccess)
         {
-            SetCachedPerson(cacheKey, result.Value);
+            _cache.Set(cacheKey, result.Value, TimeSpan.FromDays(20));
         }
 
         return result;
-    }
-
-    private void SetCachedPerson(string cacheKey, PersonDto? person)
-    {
-        _logger.LogDebug("Setting cached value: Deskpro person {cacheKey}.", cacheKey);
-        _cache.Set(cacheKey, person, TimeSpan.FromDays(20));
     }
 }
