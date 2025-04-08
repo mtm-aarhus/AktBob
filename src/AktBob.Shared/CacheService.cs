7b97ce95abf1ecb9f24b9608ac7a7d570c69bcf6
Jakob Nørager Jørgensen <jojan@aarhus.dk>
2025-04-08 10:37:42 +0200

    Adding log to the cache service

--- src/AktBob.Shared/CacheService.cs
+++ src/AktBob.Shared/CacheService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
 
 namespace AktBob.Shared;
 
@@ -11,20 +12,30 @@ public interface ICacheService
 public class CacheService : ICacheService
 {
     private readonly IMemoryCache _memoryCache;
+    private readonly ILogger<CacheService> _logger;
 
-    public CacheService(IMemoryCache memoryCache)
+    public CacheService(IMemoryCache memoryCache, ILogger<CacheService> logger)
     {
         _memoryCache = memoryCache;
+        _logger = logger;
     }
 
     public T? Get<T>(string key)
     {
+        _logger.LogDebug("Getting cache value of type {type} for key {key}", typeof(T), key);
         _memoryCache.TryGetValue(key, out T? value);
+
+        if (value == null)
+        {
+            _logger.LogDebug("Cached value of type {type} for key {key} not found", typeof(T), key);
+        }
+
         return value;
     }
 
     public void Set<T>(string key, T value, TimeSpan duration)
     {
+        _logger.LogDebug("Setting cached value with key {key} of type {type}: {value}. Expires in {duration}", key, typeof(T), value, duration);
         _memoryCache.Set(key, value, new MemoryCacheEntryOptions
         {
             AbsoluteExpirationRelativeToNow = duration
