449c5f76eb89ab9a89ae2df0f42f52d67c37dcfd
Jakob Nørager Jørgensen <jojan@aarhus.dk>
2025-04-10 09:55:25 +0200

    Add tests for Email ModuleLoggingDecorator

---
+++ test/AktBob.Email.Tests.Unit/Decorators/ModuleLoggingDecoratorTests.cs
@@ -0,0 +1,34 @@
+﻿using AktBob.Email.Contracts;
+using AktBob.Email.Decorators;
+using FluentAssertions;
+using Microsoft.Extensions.Logging.Testing;
+using NSubstitute;
+
+namespace AktBob.Email.Tests.Unit.Decorators;
+public class ModuleLoggingDecoratorTests
+{
+    private readonly ModuleLoggingDecorator _sut;
+    private readonly IEmailModule _inner = Substitute.For<IEmailModule>();
+    private readonly FakeLogger<EmailModule> _logger = new FakeLogger<EmailModule>();
+
+    public ModuleLoggingDecoratorTests()
+    {
+        _sut = new ModuleLoggingDecorator(_inner, _logger);
+    }
+
+    [Fact]
+    public void Send_ShouldCallInnerAndLogInformation_WhenInvoked()
+    {
+        // Arrange
+        var to = "to";
+        var subject = "subject";
+        var body = "body";
+
+        // Act
+        _sut.Send(to, subject, body);
+
+        // Assert
+        _inner.Received(1).Send(Arg.Is(to), Arg.Is(subject), Arg.Is(body));
+        _logger.Collector.LatestRecord.Level.Should().Be(Microsoft.Extensions.Logging.LogLevel.Information);
+    }
+}
