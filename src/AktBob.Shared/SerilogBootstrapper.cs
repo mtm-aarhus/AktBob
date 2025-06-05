ffb53e00faa8bb5427394531dcc19d87d98c599c
Jakob Nørager Jørgensen <jojan@aarhus.dk>
2025-06-05 16:08:09 +0200

    Add ErrorOrToEndpointResponse extensions

---
+++ src/AktBob.Shared/SerilogBootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+using Serilog.Events;
+
+namespace AktBob.Shared;
+
+public static class SerilogBootstrapper
+{
+    public static void ConfigureLogging(string? instrumentationKey)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .MinimumLevel.Override("System", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.ApplicationInsights(instrumentationKey, TelemetryConverter.Traces)
+            .CreateLogger();
+    }
+}
