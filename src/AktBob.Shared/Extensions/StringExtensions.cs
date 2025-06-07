40ab205c4290c490cc14683b641c3e0b57b63258
Jakob Nørager Jørgensen <jojan@aarhus.dk>
2025-06-07 13:53:09 +0200

    Move string extension TryParseDeskproDateTime to shared project

---
+++ src/AktBob.Shared/Extensions/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace AktBob.Shared.Extensions;
+
+public static class StringExtensions
+{
+    public static bool TryParseDeskproDateTime(this string? input, out DateTime? parsedDateTime)
+    {
+        var dateFormat = "yyyy-MM-dd'T'HH:mm:ssK";
+        var alternateDateFormat = "yyyy-MM-dd'T'HH:mm:ss+zzzz";  // Alternate format with no colon in offset
+
+        if (DateTime.TryParseExact(input, dateFormat, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out var date))
+        {
+            parsedDateTime = date;
+            return true;
+        }
+        else if (DateTime.TryParseExact(input, alternateDateFormat, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out date))
+        {
+            parsedDateTime = date;
+            return true;
+        }
+
+        parsedDateTime = null;
+        return false;
+    }
+}
