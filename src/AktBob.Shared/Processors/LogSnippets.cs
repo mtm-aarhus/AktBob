40b559af87e802c4ccafd229392d4050b378510c
Jakob Nørager Jørgensen <jojan@aarhus.dk>
2025-06-08 09:40:06 +0200

    Add log snippet for service bus message id and delivery count

---
+++ src/AktBob.Shared/Processors/LogSnippets.cs
@@ -0,0 +1,6 @@
+﻿namespace AktBob.Shared.Processors;
+
+public static class LogSnippets
+{
+    public static string MessageDeliveryCount(string messageId, int count) => $"Message {messageId} delivery count {count}";
+}
