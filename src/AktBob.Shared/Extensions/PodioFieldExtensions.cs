d9e7ff7bf96141764bfebdfabfdc430b84c5ba82
Jakob Nørager Jørgensen <jojan@aarhus.dk>
2025-06-16 21:10:12 +0200

    Add extension methods for deserializing messages + extracting values from Podio fields

---
+++ src/AktBob.Shared/Extensions/PodioFieldExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Nodes;
+using AktBob.Shared.Contracts.Modules.Podio;
+
+namespace AktBob.Shared.Extensions;
+
+public static class PodioFieldExtensions
+{
+    public static T? GetValue<T>(this IReadOnlyCollection<FieldDto> fields, int fieldId)
+    {
+        var fieldValue = JsonValue.Create(fields.FirstOrDefault(x => x.Id == fieldId)?.Value);
+        if (fieldValue is not (JsonNode and JsonValue valueNode)) return default;
+        return valueNode.TryGetValue<T>(out var value) ? value : default;
+    }
+}
---
+++ src/AktBob.Shared/MessageDeserializer.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+using AktBob.Shared.Exceptions;
+using AktBob.Shared.Processors;
+using Azure.Messaging.ServiceBus;
+
+namespace AktBob.Shared;
+
+public static class MessageDeserializer
+{
+    public static T Deserialize<T>(ServiceBusReceivedMessage message)
+    {
+        var job = JsonSerializer.Deserialize<T>(message.Body, SerializerConfiguration.SerializerOptions());
+        if (job is null) throw new BusinessException($"{LogSnippets.MessageDeliveryCount(message.MessageId, message.DeliveryCount)}: Message body could not be deserialized to type {typeof(T).Name}. Body content = {message.Body}");
+        return job;
+    }
+}
